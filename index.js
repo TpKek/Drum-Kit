31ed2aa286baa85ce2d224ffe9c5141f07a38413
TpK <Bertin.Dreyer1@gmail.com>
2026-02-04 14:53:22 +0200

    Restructure

---
+++ index.js
@@ -0,0 +1,15 @@
+// alert('Hello');
+
+// Event Listener With anon Function
+document.querySelector('button').addEventListener('click', function () {
+  alert('I Got Clicked');
+});
+
+// function handleClick() {
+//   alert('I Got Clicked');
+// }
+
+//what to do when you need all buttons
+//document.querySelectorAll('button');
+
+console.log('We love you');
